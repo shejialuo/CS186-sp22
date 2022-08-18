81dca6bd8f0986db080ab92e0e3e3e1d9c016bb0
shejialuo <shejialuo@gmail.com>
2022-08-18 21:16:35 +0800

    Finish Project 1 Task 1-2

--- sp22-proj1/proj1.sql
+++ sp22-proj1/proj1.sql
@@ -19,49 +19,79 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, birthYear
+  FROM people
+  WHERE weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, birthYear
+  FROM people
+  WHERE nameFirst = ' ' OR
+        nameFirst LIKE '% ' OR
+        nameFirst LIKE ' %' OR
+        nameFirst LIKE '% %'
+  ORDER BY nameFirst ASC, nameLast ASC
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthYear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthYear
+  ORDER BY birthYear ASC
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthYear, avgheight, count
+  FROM q1iii
+  WHERE avgheight > 70
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, people.playerId, yearid
+  FROM people, halloffame
+  WHERE people.playerId = halloffame.playerId AND
+        halloffame.inducted = 'Y'
+  ORDER BY yearid DESC, people.playerId ASC
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT nameFirst, nameLast, people.playerId, collegeplaying.schoolId, yearid
+  FROM people, halloffame, collegeplaying, schools
+  WHERE people.playerId = halloffame.playerId AND
+        people.playerId = collegeplaying.playerid AND
+        collegeplaying.schoolId = schools.schoolId AND
+        schools.schoolState = 'CA' AND
+        halloffame.inducted = 'Y'
+  ORDER BY yearid DESC, collegeplaying.schoolId ASC, people.playerId ASC
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT T.playerId, T.nameFirst, T.nameLast, T.schoolId
+  FROM ((SELECT playerId, nameFirst, nameLast FROM people) NATURAL JOIN
+       (SELECT playerId FROM halloffame WHERE inducted = 'Y') NATURAL LEFT OUTER JOIN
+       (SELECT playerid as playerId, schoolId FROM collegeplaying) NATURAL LEFT OUTER JOIN
+       (SELECT schoolId FROM schools)) AS T
+  ORDER BY T.playerId DESC, T.schoolId ASC
 ;
 
 -- Question 3i
